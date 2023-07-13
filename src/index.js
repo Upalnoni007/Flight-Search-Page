9ef2fd329d14390a9c26b6f772ab1d56f841064c
priyanshug01 <priyanshu945pg@gmail.com>
2023-07-13 20:34:13 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import FlightSearch from './FlightSearch';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <FlightSearch />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
